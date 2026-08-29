e04cc14028026c402316373de820657e8579fd2e
Logic Legend <ranveersinghr399@gmail.com>
2026-08-29 06:21:05 +0530

    Fix base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/icse-resources1/', // <-- Must match your repo name exactly
+  base: '/icse-resources1', // <-- Must match your repo name exactly
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
